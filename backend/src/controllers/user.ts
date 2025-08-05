b22d035fcbbf85662e2e0f434b33111afe7f3ad3
JosueGarcia04 <josueadrian042006@gmail.com>
2025-08-05 00:14:37 -0600

    filtro solo estudiantes

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -2,7 +2,11 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 export const getUsers = async (req:any, res:any)=>{
     try{
-        const users=  await prisma.user.findMany();
+        const users=  await prisma.user.findMany({
+            where:{
+                role:'estudiante'
+            }
+        });
         res.json(users);
     }catch(error){
         console.error("error fetching students", error);
